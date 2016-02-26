5116fb218003b5aa0521d1d1dca800382d735d94
陈李雨声 <540961620@qq.com>
2016-02-26 15:44:20 +0800

    当不存在二级菜单时，一级菜单可以点击

--- module/js/bootstrapNavig.js
+++ module/js/bootstrapNavig.js
@@ -28,27 +28,30 @@ jQueryEventModuleCraftConfig = typeof jQueryEventModuleCraftConfig === "undefine
 jQueryEventModuleCraftConfig = $.extend(jQueryEventModuleCraftConfig, {
     bootstrapNavig: {
         tpl:'\
-            {%for (var i = 0, len = data.length, aData, list; i < len; i++) {%}\
+            {%for (var i = 0, len = data.length, aData, list, leng; i < len; i++) {%}\
                 {%aData = data[i];%}\
                 {%list = aData[\'list\'];%}\
+                {%leng = list?list.length:0;%}\
                     <div class="panel panel-default">\
-                        <div class="panel-heading" data-toggle="collapse" href="[data-navig-id=\'collapse{%=i%}\']">\
+                        <div class="panel-heading"{%=leng > 0?\' data-toggle="collapse"\':\'\'%} href="[data-navig-id=\'collapse{%=i%}\']">\
                             <h4 class="panel-title">\
-                                <a data-key="{%=aData[\'key\']%}" class="collapsed" {%=aData[\'exAttr\']?aData[\'exAttr\']:\'\'%}>\
+                                <a data-key="{%=aData[\'key\']%}" href="{%=aData[\'url\']%}" class="collapsed" {%=aData[\'exAttr\']?aData[\'exAttr\']:\'\'%}>\
                                     {%=aData[\'title\']%}\
                                 </a>\
                             </h4>\
                         </div>\
+                        {%if(leng > 0){%}\
                         <div data-navig-id="collapse{%=i%}" class="panel-collapse collapse{%=aData[\'open\'] == true ?\' in\':\'\'%}">\
                             <div class="panel-body">\
                                 <div class="content-cta list-group" style="margin: -15px">\
-                {%for (var k = 0, leng = list.length, item; k < leng; k++) {%}\
+                {%for (var k = 0, item; k < leng; k++) {%}\
                     {%item = list[k];%}\
                                     <a data-key="{%=item[\'key\']%}" href="{%=item[\'url\']%}" class="list-group-item" {%=item[\'exAttr\']?item[\'exAttr\']:\'\'%}>{%=item[\'title\']%}</a>\
                 {%}%}\
                                 </div>\
                             </div>\
                         </div>\
+                        {%}%}\
                     </div>\
             {%}%}'
         ,
